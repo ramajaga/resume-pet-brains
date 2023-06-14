ba1970a63632e5c746d1dc030c091bb3b006d66d
unknown <agorbal@yahoo.com>
2023-06-14 19:24:03 +0300

    fixed rout task21

--- src/route/index.js
+++ src/route/index.js
@@ -1134,7 +1134,6 @@ router.get('/facebook', function (req, res) {
 })
 // ================================================================
 
-<<<<<<< HEAD
 router.get('/task21', function (req, res) {
   // res.render генерує нам HTML сторінку
 
@@ -1199,69 +1198,9 @@ router.get('/task21', function (req, res) {
           url: 'https://getbootstrap.com/',
         },
       ],
-=======
-router.get('/task22', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('task22', {
-    layout: 'basic',
-    page_header: {
-      title: 'InnovaMind',
-      description: 'Welcome to our wonderful community',
-    },
-    section: {
-      about_us: {
-        title_list: {
-          main: 'InnovaMind',
-          sub: 'InnovaMind is a forward-thinking startup focused on innovation. We are dedicated to creating efficient and powerful software through the use of first-class functions.',
-        },
-        button: { text: 'Show more' },
-      },
-      advantage: {
-        title: 'Advantage',
-        efficiency: {
-          title: 'Efficiency',
-          sub_title: 'Streamline your software development',
-          description:
-            'Streamline your software development with our innovative platform...',
-          button: 'Show more',
-        },
-        innovation: {
-          title: 'Innovation',
-          sub_title:
-            'Forward-thinking approach to programming',
-          description:
-            ' Stay ahead of the curve with our forward-thinking approach to programming...',
-          button: 'Show more',
-        },
-        collaboration: {
-          title: 'Collaboration',
-          sub_title: 'Connect with like-minded developers',
-          description:
-            'Connect with a community of like-minded developers to share knowledge and insights...',
-          button: 'Show more',
-        },
-      },
-      agreement: {
-        title: 'Terms of use',
-        info: {
-          sub_title: 'Check out our requirements',
-          detail: {
-            text: 'By using or accessing the service, you agree to be bound by the terms and conditions outlined in the agreement.',
-            button: 'Аccept',
-          },
-        },
-      },
->>>>>>> 4f46be469c836467eb0f0f98c0de1ce9e8cb96b7
     },
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
-<<<<<<< HEAD
-=======
-
-// ================================================================
->>>>>>> 4f46be469c836467eb0f0f98c0de1ce9e8cb96b7
 // Підключаємо роутер до бек-енду
 module.exports = router
